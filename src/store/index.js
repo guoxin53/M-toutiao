b140e0fbcf681f148cf670b0c5b3b2001d7c3c5e
GuoXin-99 <1072560418@qq.com>
2019-11-15 20:53:42 +0800

    初始化

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,12 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  // 声明数据
+  state: {},
+  // 改数据
+  mutations: {},
+  // 获取数据
+  actions: {},
+  // 模块功能   大项目用
+  modules: {}
 })
---
+++ src/views/video/video.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class='container'>video</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'></style>
